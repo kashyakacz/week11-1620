6f4284de55695afa2ad6ef4ce83e7449d227a15c
Kashya Kaczkowska <Kashya.Kacz@yahoo.ca>
2021-03-26 18:45:11 -0700

    Linked the CSS directory and JavaScript in the JS directory to index.html

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,11 @@
 // your JavaScript goes here, you can delete this comment
+let todoList = {todo: "clone repo for starter code", status: "complete, started"};
+let completedTodos={};
+
+let todoList_length = todoList.length
+
+for (let i =0; i < todoList_length; i++) {
+    if todoList[i] == 'complete' {
+        completedTodos == todoList[index]
+
+}
