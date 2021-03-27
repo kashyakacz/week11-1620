78e26e330baea98f44f9a6970494587b61adf1eb
Kashya Kaczkowska <Kashya.Kacz@yahoo.ca>
2021-03-26 18:49:29 -0700

    Created an second array to iliterate over second array

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,15 @@
 // your JavaScript goes here, you can delete this comment
-let todoList = {todo: "clone repo for starter code", status: "complete, started"};
-let completedTodos={};
+let todoList = { todo: "clone repo for starter code", status: "complete, started" };
+let completedTodos = {};
 
-let todoList_length = todoList.length
 
-for (let i =0; i < todoList_length; i++) {
-    if todoList[i] == 'complete' {
+for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i] === 'complete') {
         completedTodos == todoList[index]
 
+    }
+}
+
+for (let i = 0; i< completedTodos.length; i++) {
+    console.log(i)
 }
